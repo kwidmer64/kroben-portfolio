8c7dd40a81043995b16f0e3d389116878b5fd35c
Korben Widmer <korbenwids@gmail.com>
2025-11-11 17:28:26 -0700

    Add custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<div class="flex flex-col items-center mt-25 gap-5">
+	<h1 class="text-5xl font-bold">Something went wrong!</h1>
+<p class="text-3xl font-semibold">{page.status} - {page.error?.message}</p> <!-- ? used to if error is null -->
+</div>
