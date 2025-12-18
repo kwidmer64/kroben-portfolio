5d6b078ae21e00f2c9da7f5e4909b3d2fd13ff01
Korben Widmer <korbenwids@gmail.com>
2025-12-18 15:51:30 -0500

    Add project icons

---
+++ src/lib/icons.ts
@@ -0,0 +1,22 @@
+export default {
+	react: {
+		hovered: 'https://cdn.simpleicons.org/react/61DAFB',
+		static: 'https://cdn.simpleicons.org/react/676767'
+	},
+	js: {
+		hovered: 'https://cdn.simpleicons.org/javascript/F7DF1E',
+		static: 'https://cdn.simpleicons.org/javascript/676767'
+	},
+	nodejs: {
+		hovered: 'https://cdn.simpleicons.org/nodedotjs/5FA04E',
+		static: 'https://cdn.simpleicons.org/nodedotjs/676767'
+	},
+	svelte: {
+		hovered: 'https://cdn.simpleicons.org/svelte/FF3E00',
+		static: 'https://cdn.simpleicons.org/svelte/676767'
+	},
+	ts: {
+		hovered: 'https://cdn.simpleicons.org/typescript/3178C6',
+		static: 'https://cdn.simpleicons.org/typescript/676767'
+	}
+} as const;
