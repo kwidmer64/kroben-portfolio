d933f3dd5b6043478b890994dac66881344ae1cb
Korben Widmer <korbenwids@gmail.com>
2025-12-19 18:18:52 -0500

    force footer to bottom of the page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -50,39 +50,43 @@
 <!--	<link rel="icon" href={favicon} />-->
 <!--</svelte:head>-->
 
-<nav class="h-20 flex justify-between relative top-0 z-10">
-	<a href="/" class="w-1/2 text-3xl font-bold h-full flex items-center justify-start pl-7 lg:pl-20">KW</a>
-	<!-- Desktop Nav -->
-	<div class="w-4xl flex justify-around items-center z-0">
-		<div class="relative">
-			<a href="/" class="text-neutral-100 z-20 relative" onmouseenter={handleNavHover} onmouseleave={handleNavHover}>Home</a>
-			<div class="link-block z-10"></div>
+<div class="flex flex-col min-h-[100vh]">
+	<nav class="h-20 flex justify-between relative top-0 z-10">
+		<a href="/" class="w-1/2 text-3xl font-bold h-full flex items-center justify-start pl-7 lg:pl-20">KW</a>
+		<!-- Desktop Nav -->
+		<div class="w-4xl flex justify-around items-center z-0">
+			<div class="relative">
+				<a href="/" class="text-neutral-100 z-20 relative" onmouseenter={handleNavHover} onmouseleave={handleNavHover}>Home</a>
+				<div class="link-block z-10"></div>
+			</div>
+			<div class="relative">
+				<a href="/projects" class="text-neutral-100 z-20 relative" onmouseenter={handleNavHover} onmouseleave={handleNavHover}>Projects</a>
+				<div class="link-block z-10"></div>
+			</div>
+			<div class="relative">
+				<a href="/experience" class="text-neutral-100 z-20 relative" onmouseenter={handleNavHover} onmouseleave={handleNavHover}>Experience</a>
+				<div class="link-block z-10"></div>
+			</div>
 		</div>
-		<div class="relative">
-			<a href="/projects" class="text-neutral-100 z-20 relative" onmouseenter={handleNavHover} onmouseleave={handleNavHover}>Projects</a>
-			<div class="link-block z-10"></div>
-		</div>
-		<div class="relative">
-			<a href="/experience" class="text-neutral-100 z-20 relative" onmouseenter={handleNavHover} onmouseleave={handleNavHover}>Experience</a>
-			<div class="link-block z-10"></div>
-		</div>
-	</div>
 
-<!--	<div class="flex items-center justify-center md:hidden px-10">-->
-<!--		<button onclick={toggleNav} aria-label="Toggle Navigation">-->
-<!--			<i class="fa-solid fa-bars hover:cursor-pointer"></i>-->
-<!--		</button>-->
-<!--		&lt;!&ndash; Mobile Nav Links &ndash;&gt;-->
-<!--		<div class={`${showNav ? 'flex' : 'hidden'}`}>-->
-<!--			<a href="/" class="text-neutral-100">Home</a>-->
-<!--			<a href="/about" class="text-neutral-100">About</a>-->
-<!--			<a href="/projects" class="text-neutral-100">Projects</a>-->
-<!--		</div>-->
-<!--	</div>-->
-</nav>
+	<!--	<div class="flex items-center justify-center md:hidden px-10">-->
+	<!--		<button onclick={toggleNav} aria-label="Toggle Navigation">-->
+	<!--			<i class="fa-solid fa-bars hover:cursor-pointer"></i>-->
+	<!--		</button>-->
+	<!--		&lt;!&ndash; Mobile Nav Links &ndash;&gt;-->
+	<!--		<div class={`${showNav ? 'flex' : 'hidden'}`}>-->
+	<!--			<a href="/" class="text-neutral-100">Home</a>-->
+	<!--			<a href="/about" class="text-neutral-100">About</a>-->
+	<!--			<a href="/projects" class="text-neutral-100">Projects</a>-->
+	<!--		</div>-->
+	<!--	</div>-->
+	</nav>
 
-{@render children?.()}
+	<main class="flex-1">
+		{@render children?.()}
+	</main>
 
-<footer class="flex justify-center items-center h-20 bg-zinc-900">
-	Copyright 2025 - Korben Widmer
-</footer>
+	<footer class="flex justify-center items-center h-20 bg-zinc-900 bottom-0 left-0 right-0 relative">
+		Copyright 2025 - Korben Widmer
+	</footer>
+</div>
