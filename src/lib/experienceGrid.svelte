f173b6859a4885a7e79e4c5b857f0888f53cbf06
Korben Widmer <korbenwids@gmail.com>
2025-12-19 18:10:48 -0500

    Add experience components

---
+++ src/lib/experienceGrid.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+import Experience from './experienceIndividual.svelte';
+</script>
+
+<div class="flex flex-col lg:grid lg:grid-cols-1 px-10 md:px-40 lg:px-25 xl:px-60 gap-4 h-full">
+	<h1 class="text-5xl font-bold mb-6">Experience</h1>
+
+	<Experience title="Events A/V Student Worker" company="Grand Canyon University" skills={['Dante']} start=" August 2025" end="Present">
+		<p>Set up and struck audio and lighting systems for GCU Arena events, including chapels, concerts, and athletics.
+			Mixed live audio, managed SMIs, and coordinated with event staff to ensure smooth A/V operations.
+			Spotlight operation for NCAA Division I athletic events</p>
+	</Experience>
+
+	<Experience title="Contract Audio Engineer" company="Cornerstone Church" skills={['Dante', 'Audio Mixing', 'Midas M32', 'Waves SoundGrid']} start="March 2025" end="Present">
+		<p>Set up and struck audio and lighting systems for GCU Arena events, including chapels, concerts, and athletics. Mixed live audio, managed SMIs, and coordinated with event staff to ensure smooth A/V operations. Spotlight operation for NCAA Division I athletic events</p>
+	</Experience>
+
+	<Experience title="AVL Tech" company="Hume Lake Christian Camps" skills={['Dante', 'Audio Mixing', 'Allen & Heath Avantis', 'QLab Programming']} start=" May 2025" end="August 2025">
+		<p>Mixed live audio for daily skits and presentations, edited recordings, configured wireless audio gear. Troubleshot MIDI, QLab, ProPresenter, and Chroma-Q Vista systems, and managed Dante audio devices for seamless performances.</p>
+	</Experience>
+
+	<Experience title="Production Sound Tech" company="Hume Lake Christian Camps" skills={['Dante', 'Audio Mixing', 'Roland M5000']} start=" May 2024" end="August 2024">
+		<p>Mixed live audio for daily skits and sermons, configured wireless audio and networking gear. Communicated with the camp director to ensure audio requirements for the camp are fulfilled</p>
+	</Experience>
+</div>
---
+++ src/lib/experienceIndividual.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let title: string = '', company: string = '', start: string = '', end: string = '';
+	export let skills: string[] = [];
+</script>
+
+<div class="grid grid-rows-5 2xl:grid-rows-4 gap-2 grow min-h-50 p-4 pl-6 border-2 border-zinc-800 hover:border-[#2E86AB] hover:bg-[#111B2D]/25 rounded-lg overflow-clip z-0 transition-all duration-250 hover:shadow-lg/25 shadow-[#2E86AB] hover:-translate-y-1">
+	<h2 class="text-3xl font-bold text-zinc-200">{title}</h2>
+	<h3 class="text-xl text-[#2E86AB]">{company}</h3>
+	<p class="row-span-3 2xl:row-span-2 font-medium text-zinc-400  border-b-1 border-b-zinc-800">
+		<slot />
+	</p>
+	<div class="flex justify-between min-h-7 gap-4">
+		<div>
+			Skills: {skills.join(', ')}
+		</div>
+		<div>
+			{start} - {end}
+		</div>
+	</div>
+</div>
