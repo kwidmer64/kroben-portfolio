7f08b11e8d2dc6ae8aa89f50b10d378a94191d71
Korben Widmer <korbenwids@gmail.com>
2025-12-19 16:16:31 -0500

    Update static icons colors

--- src/lib/icons.ts
+++ src/lib/icons.ts
@@ -1,22 +1,22 @@
 export default {
 	react: {
 		hovered: 'https://cdn.simpleicons.org/react/61DAFB',
-		static: 'https://cdn.simpleicons.org/react/676767'
+		static: 'https://cdn.simpleicons.org/react/27272A'
 	},
 	js: {
 		hovered: 'https://cdn.simpleicons.org/javascript/F7DF1E',
-		static: 'https://cdn.simpleicons.org/javascript/676767'
+		static: 'https://cdn.simpleicons.org/javascript/27272A'
 	},
 	nodejs: {
 		hovered: 'https://cdn.simpleicons.org/nodedotjs/5FA04E',
-		static: 'https://cdn.simpleicons.org/nodedotjs/676767'
+		static: 'https://cdn.simpleicons.org/nodedotjs/27272A'
 	},
 	svelte: {
 		hovered: 'https://cdn.simpleicons.org/svelte/FF3E00',
-		static: 'https://cdn.simpleicons.org/svelte/676767'
+		static: 'https://cdn.simpleicons.org/svelte/27272A'
 	},
 	ts: {
 		hovered: 'https://cdn.simpleicons.org/typescript/3178C6',
-		static: 'https://cdn.simpleicons.org/typescript/676767'
+		static: 'https://cdn.simpleicons.org/typescript/27272A'
 	}
 } as const;
